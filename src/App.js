8d9c0ad61a67711baba2e4958d1ae31f16a713f7
manjunadhb <manjunadhb@gmail.com>
2024-08-29 17:31:10 +0530

    added second heading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>This line is added by Manjunadh from github.com</h1>
+      <h1>This line is added by Manjunadh from Local repository</h1>
     </div>
   );
 }
